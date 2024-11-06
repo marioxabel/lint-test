0ff441f8128360976426b89b89959989c471b401
Mario <31260739+marioxabel@users.noreply.github.com>
2024-11-05 19:06:27 -0600

    test

---
+++ src/components/Advertisement.tsx
@@ -0,0 +1,18 @@
+interface AdvertisementProps {
+  companyName: string;
+  productDescription: string;
+  price: string;
+};
+
+
+function Advertisement(props: AdvertisementProps) {
+  return (
+    <div className="advertisement">
+      <p>{props.companyName}</p>
+      <p>{props.productDescription}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+}
+
+export default Advertisement;
